410c09229ccd45f6f79030d3bf1361c83c3044c5
Dominika Fric <dominika.fric@synapse.com>
2019-12-16 17:39:00 +0100

    cors mapping

--- src/main/java/sk/cata/cursedvault/config/Config.java
+++ src/main/java/sk/cata/cursedvault/config/Config.java
@@ -2,6 +2,8 @@ package sk.cata.cursedvault.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +11,17 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class Config {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");//update this, this is not nice
+            }
+        };
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
